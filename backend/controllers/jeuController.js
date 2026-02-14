5528014fc60247416253cf67ee332446eb661fe5
kanteYoussoufAziz <kanteyoussoufaziz@gmail.com>
2026-02-14 14:06:00 +0000

    Fix: Revert Jurisprudence to 10 questions (5 pts each)

--- backend/controllers/jeuController.js
+++ backend/controllers/jeuController.js
@@ -270,7 +270,7 @@ const getQuestionsRestantes = async (req, res, next) => {
                 CASE 
                     WHEN nom = 'Culture générale' THEN 10
                     WHEN nom = 'Vie du Prophète' THEN 5
-                    WHEN nom = 'Jurisprudence' THEN 5
+                    WHEN nom = 'Jurisprudence' THEN 10
                     WHEN nom = 'Questions relais' THEN 3
                     WHEN nom = 'Hadith' THEN 1
                     ELSE 1
